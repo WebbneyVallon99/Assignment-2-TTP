40d1e76059824cfc544282eb3aa723eb1eb32752
Webbney Vallon <webbvallon1999@gmail.com>
2025-06-17 14:27:39 -0400

    added base template

---
+++ script.js
@@ -0,0 +1,47 @@
+function myForEach(arr, func) {
+  
+}
+
+function myMap(arr, func) {
+  
+}
+
+function myFilter(arr, func) {
+  
+}
+
+function mySome(arr, func) {
+  
+}
+
+function myEvery(arr, func) {
+  
+}
+
+function myReduce(arr, func) {
+  
+}
+
+function myIncludes(arr, func) {
+  
+}
+
+function myIndexOf(arr, func) {
+  
+}
+
+function myPush(arr, func) {
+  
+}
+
+function myLastIndexOf(arr, func) {
+  
+}
+
+function myObjectKeys(obj, func) {
+  
+}
+
+function myObjectValues(obj, func) {
+  
+}
