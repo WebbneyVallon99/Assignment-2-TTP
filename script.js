42dc851448e94f9a24b3ef38419405d7a2919365
Webbney Vallon <webbvallon1999@gmail.com>
2025-06-17 20:41:35 -0400

    implemented all functions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function myForEach(arr, func) {
 
 let numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach(function(number) {
+myForEach(numbers,function(number) {
   console.log(number); // 1 2 3 4 5
 });
 
@@ -20,32 +20,84 @@ function myFilter(arr, func) {
 }
 
 function mySome(arr, func) {
-  webb
+    for (let j = 0; j < arr.length; j++){
+        if (func(arr[j]))
+            return true;
+    }
+    return false;
 }
 
+let hasEvenNumber = mySome(numbers,function(num) {
+  return num % 2 === 0;
+});
+console.log(hasEvenNumber); // true
+
+
 function myEvery(arr, func) {
-  webb
+  for (let w = 0; w < arr.length; w++) {
+    if (func(arr[w]))
+        return true;
+  }
+  return false;
 }
+
+
+let Everynumber = [2, 4, 6, 8, 10];
+let allNumbersEven = myEvery(Everynumber,function(num) {
+  return num % 2 === 0;
+});
+console.log(allNumbersEven); // true
 
 function myReduce(arr, func) {
   
 }
 
-function myIncludes(arr, func) {
-  webb
+function myIncludes(arr, target) {
+  for (let v = 0; v < arr.length; v++){
+    if (arr[v] === target)
+        return true;
+  }
+  return false;
 }
 
-function myIndexOf(arr, func) {
-  webb
+const fruits = ['apple', 'banana', 'orange', 'grape'];
+const hasBanana = myIncludes(fruits,'banana');
+console.log(hasBanana); // true
+
+
+function myIndexOf(arr, target) {
+  for (let d = 0; d < arr.length; d++){
+    if (arr[d] === target)
+        return d;
+  }
+  return -1;
 }
+
+const indexOrange = myIndexOf(fruits,'orange');
+console.log(indexOrange); // 2
+
+const indexPear = myIndexOf(fruits,'pear');
+console.log(indexPear); // -1
 
 function myPush(arr, func) {
   
 }
 
-function myLastIndexOf(arr, func) {
-  webb
+function myLastIndexOf(arr, target) {
+ for (let n = arr.length - 1; n >= 0; n--){
+    if (arr[n] === target)
+        return n;
+ }
+ return -1;
 }
+  
+const animals = ['cat', 'dog', 'bird', 'dog', 'fish'];
+
+const lastIndexDog = myLastIndexOf(animals,'dog');
+console.log(lastIndexDog); // 3
+
+const lastIndexCow = myLastIndexOf(animals,'cow');
+console.log(lastIndexCow); // -1
 
 function myObjectKeys(obj, func) {
   
